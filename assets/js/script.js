77f81840e5a954499f568177d92999e8b1fe3dc2
michelle29ahn <michelleahn0418@berkeley.edu>
2020-12-04 23:26:51 -0800

    final project for wdd

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "200px";
+    document.getElementById("main").style.marginRight = "200px";
+    document.getElementById("mySidebar").style.display="block";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+    document.getElementById("mySidebar").style.display="none";
+  }
+
